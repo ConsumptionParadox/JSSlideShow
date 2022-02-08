3fcd9d35b853af5f0c3483a303db78267401d1c8
ConsumeEm <98207055+ConsumptionParadox@users.noreply.github.com>
2022-02-08 18:10:34 -0500

    Template Done

---
+++ script.js
@@ -0,0 +1,29 @@
+var index = 0;
+
+show_image(index)
+
+function show_image(i){
+    index+=i;
+
+    let images = document.getElementsByClassName("image");
+
+    let dots = document.getElementsByClassName("dot");
+
+    //block images from showing on screen to prevent bug
+    for(i=0;i<images.length;i++)
+        images[i].style.display = "none";
+
+    for(i=0;i<dots.length;i++)
+        dots[i].className = dots[i].className.replace(" active", "");
+
+    //user clicks next while on last image
+    if(index > images.length - 1)
+        index = 0;
+    
+    //user clicks prev while on first image
+    if(index < 0)
+        index = images.length - 1;
+
+    images[index].style.display = "block";
+    dots[index].className += " active";
+}
